6c6aeb2ac30052929c783b00d7f2ba1344544564
Brandon <brandonkly@ucla.edu>
2022-10-15 00:17:38 -0700

    formatted code to be more readable

--- script.js
+++ script.js
@@ -1,68 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var uppercase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-];
+var uppercase = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
 var lowercase = uppercase.map((letter) => letter.toLowerCase());
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "-",
-  "_",
-  "=",
-  "+",
-  "[",
-  "]",
-  "{",
-  "}",
-  "|",
-  ";",
-  ":",
-  "'",
-  ",",
-  ".",
-  "<",
-  ">",
-  "/",
-  "?",
-  "~",
-  "`"
-];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`" ];
 
 // Write password to the #password input
 function writePassword() {
@@ -79,21 +21,13 @@ function generatePassword() {
   var password = "";
   // if statement to ensure user enters correct length
   // prompt for password length: 8-128
-  var passwordLength = prompt(
-    "How many characters would you like your password to be? (8-128)"
-  );
+  var passwordLength = prompt("How many characters would you like your password to be? (8-128)");
   if (passwordLength >= 8 && passwordLength <= 128) {
     // prompt for password criteria: upper, lower, number, special
-    var isUppercase = confirm(
-      "Would you like uppercase letters in your password?"
-    );
-    var isLowercase = confirm(
-      "Would you like lowercase letters in your password?"
-    );
+    var isUppercase = confirm("Would you like uppercase letters in your password?") ;
+    var isLowercase = confirm("Would you like lowercase letters in your password?");
     var isNumber = confirm("Would you like numbers in your password?");
-    var isSpecial = confirm(
-      "Would you like special characters in your password?"
-    );
+    var isSpecial = confirm("Would you like special characters in your password?");
 
     // use if statement to add criteria arrays to passwordText
     var includedCharacters = [];
@@ -117,7 +51,6 @@ function generatePassword() {
     }
     // console.log(includedCharacters);
 
-    // i =1 to get same # as passwordLength
     for (i = 0; i < passwordLength; i++) {
       // randomize letters from includedCharacters by using random numbers to choose array value
       password = password + includedCharacters[getRandomInt(0, includedCharacters.length-1, includedCharacters)];
