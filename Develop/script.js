99ff99f27f2584b83404ef53ac937a2b233a607d
Brandon <brandonkly@ucla.edu>
2022-10-14 18:08:43 -0700

    added prompts for criteria and password length

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,6 @@ var lowercase = uppercase.map(letter => letter.toLowerCase())
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,3 +17,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// prompt for password criteria: upper, lower, number, special
+var isUppercase = prompt("Would you like uppercase letters in your password?")
+var isLowercase = prompt("Would you like lowercase letters in your password?")
+var isNumber = prompt("Would you like numbers in your password?")
+var isSpecial = prompt("Would you like special characters in your password?")
+// prompt for password length: 8-128
+var passwordLength = prompt("How many characters would you like your password to be? (8-128)")
