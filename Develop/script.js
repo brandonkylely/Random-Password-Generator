240465356852c262c2be249a8d698b8b21d41ec7
Brandon <brandonkly@ucla.edu>
2022-10-14 18:22:47 -0700

    created generate password function, added concat statements

--- Develop/script.js
+++ Develop/script.js
@@ -18,10 +18,30 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+function generatePassword () { 
 // prompt for password criteria: upper, lower, number, special
-var isUppercase = confirm("Would you like uppercase letters in your password?")
-var isLowercase = confirm("Would you like lowercase letters in your password?")
-var isNumber = confirm("Would you like numbers in your password?")
-var isSpecial = confirm("Would you like special characters in your password?")
+  var isUppercase = confirm("Would you like uppercase letters in your password?")
+  var isLowercase = confirm("Would you like lowercase letters in your password?")
+  var isNumber = confirm("Would you like numbers in your password?")
+  var isSpecial = confirm("Would you like special characters in your password?")
 // prompt for password length: 8-128
-var passwordLength = prompt("How many characters would you like your password to be? (8-128)")
+  var passwordLength = prompt("How many characters would you like your password to be? (8-128)")
+
+// use if statement to add criteria arrays to passwordText
+
+  if (isUppercase) {
+    uppercase.concat(passwordText)
+  }
+  if (isLowercase) {
+    lowercase.concat(passwordText)
+  }
+  if (isNumber) {
+    numbers.concat(passwordText)
+  }
+  if (isNumber) {
+    special.concat(passwordText)
+  }
+}
+
+generatePassword()
